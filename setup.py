d9febc318f0f270c6eb132563bb1583a61377552
TitouanGuerin <titouanguerin@gmail.com>
2026-03-03 14:12:00 +0100

    modified slightly requirements to make it easier to install

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
 setup(name='fw_flightcontrol', 
       version='0.0.1',
       packages=find_packages(),
+      install_requires=requirements
       )
